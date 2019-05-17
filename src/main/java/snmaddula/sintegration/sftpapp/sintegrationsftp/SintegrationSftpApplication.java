58870f2b4644823774be6f85ddb82103bc5e2a1a
snmaddula <snmaddula@gmail.com>
2019-05-17 08:51:22 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/sintegration/sftpapp/sintegrationsftp/SintegrationSftpApplication.java
@@ -0,0 +1,13 @@
+package snmaddula.sintegration.sftpapp.sintegrationsftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SintegrationSftpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SintegrationSftpApplication.class, args);
+	}
+
+}
